f98ac20375f5935d1e561cb1ced28e80495447d4
Michael L Perry <michael@qedcode.com>
2014-02-25 10:55:05 -0600

    Added JUnit and BouncyCastle to project.

---
+++ CryptoFundamentalsTests/src/cryptofundamentals/tests/SymmetricTests.java
@@ -0,0 +1,14 @@
+package cryptofundamentals.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SymmetricTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
